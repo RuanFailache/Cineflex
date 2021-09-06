d4297d6271d072287ad09e20ce8a22bcf2d49175
RuanFailache <ruanfailache@outlook.com>
2021-09-05 21:03:58 -0300

    Implementation of conditions on data's subimit on user form

--- src/components/Seats/UserForm.js
+++ src/components/Seats/UserForm.js
@@ -2,20 +2,47 @@ import { Form } from "./style-seat";
 
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function UserForm({ selectedSeats, info, setInfos }) {
   const [name, setName] = useState("");
   const [cpf, setCpf] = useState("");
+  const [canChange, setCanChange] = useState(false);
+
+  useEffect(() => {
+    const regexCpf = new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/);
+    const seats = selectedSeats.map((seat) => seat.name);
+
+    if (seats.length === 0 || name === "" || cpf === "" || !regexCpf.test(cpf))
+      return;
+    setCanChange(true);
+  }, [name, cpf, selectedSeats]);
 
   function chooseSeats() {
+    const regexCpf = new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/);
+    const seats = selectedSeats.map((seat) => seat.name);
+
+    if (seats.length === 0) {
+      alert("Selecione pelo menos um assento!");
+      return;
+    } else if (name === "") {
+      alert("Preencha o seu nome!");
+      return;
+    } else if (cpf === "") {
+      alert("Preencha o seu cpf!");
+      return;
+    } else if (!regexCpf.test(cpf)) {
+      alert("Preencha o seu cpf no seguinte formato!\n'xxx.xxx.xxx-xx'");
+      return;
+    }
+
     setInfos({
       title: info.movie.title,
       date: info.day.date,
       hour: info.name,
-      name: name,
-      cpf: cpf,
-      seats: selectedSeats.map((seat) => seat.name),
+      name,
+      cpf,
+      seats,
     });
 
     axios.post(
@@ -44,7 +71,7 @@ export default function UserForm({ selectedSeats, info, setInfos }) {
         placeholder="Digite seu nome"
       />
 
-      <Link to="/sucesso" onClick={chooseSeats}>
+      <Link to={canChange ? "/sucesso" : "#"} onClick={chooseSeats}>
         Reservar seu assento
       </Link>
     </Form>
